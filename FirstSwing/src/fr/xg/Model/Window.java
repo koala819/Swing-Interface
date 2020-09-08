9b6c8788e28260adead04d87b1b19916a89bae98
koala <koala@FreeAndFun>
2020-09-08 17:03:15 +0200

    JRadioButton & JCheckBox

--- FirstSwing/src/fr/xg/Model/Window.java
+++ FirstSwing/src/fr/xg/Model/Window.java
@@ -1,25 +1,29 @@
 package fr.xg.Model;
 
-import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JFileChooser;
+import javax.swing.Box;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JSplitPane;
 
-public class Fenetre
+public class Window
 {
 	protected static Component frame;
 
-	public static void Fenetre() {
+	public static void Window() {
 		//Création barre de menu
 		JMenuBar menuBar = new JMenuBar();
 		JMenu menu = new JMenu("Fichier");
@@ -40,7 +44,7 @@ public class Fenetre
 		//Création de la fenêtre 
 		JFrame frame = new JFrame();
 		frame.setTitle("Bienvenue");
-		frame.setSize(400, 300);
+		frame.setSize(400, 600);
 		frame.setResizable(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setJMenuBar(menuBar);
@@ -49,15 +53,45 @@ public class Fenetre
 
 
 		/*Contenu de la fenêtre avec deux zones
-		 * zone gauche :: zone_outil (checkbox & radio boutons)
+		 * zone gauche :: zone_outil (JCheckBox & JRadioButton)
 		 * zone droite :: zone_dessin
 		 * */
 		JPanel zone_outil= new JPanel();
 	    JPanel zone_dessin = new JPanel();
 	    Container container = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
 	    zone_outil.setBackground(Color.ORANGE);
+	    zone_outil.setLayout(new GridLayout(3, 1));
 	    zone_dessin.setBackground(Color.DARK_GRAY);
 	    container.add(zone_outil);
+	    
+	    //Création JCheckBox
+	    JCheckBox logging = new JCheckBox("Enable logging");
+	    JCheckBox sound = new JCheckBox("Increase sound");
+	    JCheckBox photo = new JCheckBox("Take photo");
+	    Box boxVerticale = Box.createVerticalBox();
+	    boxVerticale.add(logging);
+	    boxVerticale.add(sound);
+	    boxVerticale.add(photo);
+	    zone_outil.add(boxVerticale);
+	    
+	    //Création JRadioButton
+	    JLabel lblMessage = new JLabel("How much coffee do you want ?");
+	    JRadioButton j1 = new JRadioButton("One");
+	    JRadioButton j2 = new JRadioButton("Two");
+	    JRadioButton j3 = new JRadioButton("Three");
+	    //Pour ne sélectionner qu'un bouton radio
+	    ButtonGroup G1 = new ButtonGroup();
+	    Box boxHorizontal = Box.createHorizontalBox();
+	    G1.add(j1);
+	    G1.add(j2);
+	    G1.add(j3);
+	    
+	    zone_outil.add(lblMessage);
+	    boxHorizontal.add(j1);
+	    boxHorizontal.add(j2);
+	    boxHorizontal.add(j3);
+	    zone_outil.add(boxHorizontal);
+	    
 	    container.add(zone_dessin);
 	    frame.getContentPane().add(container);
 	    
